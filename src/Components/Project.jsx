9f92f0082f7ab4ca44a08aaf6d056b66351a1139
MickaelCrea <130543184+Mickaelcrea@users.noreply.github.com>
2023-05-17 09:24:46 +0000

    Update Project.jsx and navigation.module.css

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -4,7 +4,7 @@ export function Project(props) {
     console.log(props)
     return (
         <article className="card bg-light p-2" style={{ width: "18rem" }}>
-            <img src={props.project.image.src} alt="props.project.image.src" />
+            <img src={props.project.image.src} alt={props.project.image.src} />
             <h3>{props.project.title}</h3>
             <p>{props.project.software}</p>
         </article>
